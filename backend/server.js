e894fd3d88896d96421acf2a564799d257565e4b
dharamdan01 <dharamdan01@gmail.com>
2026-07-04 21:14:40 +0500

    Update greeting message in server response

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world zello app</h1>');
+  res.send('<h1>Hello world zello</h1>');
 });
 
 server.listen(7000, () => {
